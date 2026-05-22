7fe0f6c705db3009e9ab68dc9aca1ac3941bb601
nO3o <86451057+nO3o@users.noreply.github.com>
2026-05-22 13:19:08 +0900

    feat: PCH 및 윈도우 초기화 시점에 디버그용 메모리 누수 탐지 매크로 추가
    - 디버그 모드(_DEBUG)에서 메모리 할당 위치를 추적할 수 있도록 PCH에 crtdbg.h 매크로 정의
    - new 연산자를 DBG_NEW로 재정의하여 할당된 소스 파일과 라인 넘버가 출력되도록 설정
    - 윈도우 초기화 시점에 _CrtSetDbgFlag를 추가하여 프로그램 종료 시 자동으로 메모리 누수를 감지하도록 구현

--- Source/00. Main/PCH/pch.h
+++ Source/00. Main/PCH/pch.h
@@ -10,5 +10,18 @@
 // 여기에 미리 컴파일하려는 헤더 추가
 #include "../framework.h"
 
+#ifdef _DEBUG
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#ifndef DBG_NEW 
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ ) 
+#define new DBG_NEW 
+
+#endif
+#endif
+
 
 #endif //PCH_H
